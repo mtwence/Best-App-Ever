ff4abe56bed8d09b90086ff1fd8c35c34bb6e563
zzzorigtbaatar <zzzorigtbaatar@gmail.com>
2022-08-04 11:31:30 -0700

    add index model relations player_game

--- models/index.js
+++ models/index.js
@@ -1,20 +1,29 @@
-const Player = require('./Player');
-const Tournament = require('./Tournament');
+const Player = require("./Player");
+const Tournament = require("./Tournament");
+const Game = require("./Game");
 
 Player.belongsToMany(Tournament, {
-    through: "player_tournament",
-    as: "tournaments",
-    foreignKey: "player_id",
-    onDelete: 'CASCADE'
-  });
+  through: "player_tournament",
+  as: "tournaments",
+  foreignKey: "player_id",
+});
 
 Tournament.belongsToMany(Player, {
   through: "player_tournament",
   as: "player",
   foreignKey: "tournament_id",
-  onDelete: 'CASCADE'
 });
 
+Player.belongsToMany(Game, {
+  through: "player_game",
+  as: "games",
+  foreignKey: "player_id",
+});
 
+Game.belongsToMany(Player, {
+  through: "player_game",
+  as: "players",
+  foreignKey: "game_id",
+});
 
-module.exports = { Player, Tournament };
+module.exports = { Player, Tournament, Game };
