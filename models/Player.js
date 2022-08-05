27cbf557f3e24938f47fb690b79c1443d3efdeb8
zzzorigtbaatar <zzzorigtbaatar@gmail.com>
2022-08-05 11:18:45 -0700

    add starter files

--- models/Player.js
+++ models/Player.js
@@ -16,18 +16,18 @@ Player.init(
       primaryKey: true,
       autoIncrement: true,
     },
-    username: {
+    name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
-    // email: {
-    //   type: DataTypes.STRING,
-    //   allowNull: false,
-    //   unique: true,
-    //   validate: {
-    //     isEmail: true,
-    //   },
-    // },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+      },
+    },
     password: {
       type: DataTypes.STRING,
       allowNull: false,
@@ -35,31 +35,78 @@ Player.init(
         len: [8],
       },
     },
-    favoriteGames: {
-      type: DataTypes.ARRAY,
-      allowNull: true,
-    },
   },
   {
     hooks: {
-      beforeCreate: async (newPlayerData) => {
-        newPlayerData.password = await bcrypt.hash(newPlayerData.password, 10);
-        return newPlayerData;
+      beforeCreate: async (newUserData) => {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
       },
-      beforeUpdate: async (updatedPlayerData) => {
-        updatedPlayerData.password = await bcrypt.hash(
-          updatedPlayerData.password,
+      beforeUpdate: async (updatedUserData) => {
+        updatedUserData.password = await bcrypt.hash(
+          updatedUserData.password,
           10
         );
-        return updatedPlayerData;
+        return updatedUserData;
       },
     },
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: "Player",
+    modelName: "user",
   }
+  // {
+  //   id: {
+  //     type: DataTypes.INTEGER,
+  //     allowNull: false,
+  //     primaryKey: true,
+  //     autoIncrement: true,
+  //   },
+  //   username: {
+  //     type: DataTypes.STRING,
+  //     allowNull: false,
+  //   },
+  //   email: {
+  //     type: DataTypes.STRING,
+  //     allowNull: false,
+  //     unique: true,
+  //     validate: {
+  //       isEmail: true,
+  //     },
+  //   },
+  //   password: {
+  //     type: DataTypes.STRING,
+  //     allowNull: false,
+  //     validate: {
+  //       len: [8],
+  //     },
+  //   },
+  //   favoriteGames: {
+  //     type: DataTypes.ARRAY(DataTypes.STRING),
+  //     allowNull: true,
+  //   },
+  // },
+  // {
+  //   hooks: {
+  //     beforeCreate: async (newPlayerData) => {
+  //       newPlayerData.password = await bcrypt.hash(newPlayerData.password, 10);
+  //       return newPlayerData;
+  //     },
+  //     beforeUpdate: async (updatedPlayerData) => {
+  //       updatedPlayerData.password = await bcrypt.hash(
+  //         updatedPlayerData.password,
+  //         10
+  //       );
+  //       return updatedPlayerData;
+  //     },
+  //   },
+  //   sequelize,
+  //   timestamps: false,
+  //   freezeTableName: true,
+  //   underscored: true,
+  //   modelName: "Player",
+  // }
 );
 
 module.exports = Player;
