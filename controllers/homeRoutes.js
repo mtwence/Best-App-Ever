05b6ef193a3cf7e0adffab9636124d13966d3d5d
zzzorigtbaatar <zzzorigtbaatar@gmail.com>
2022-08-06 14:36:55 -0700

    added Michael's route back

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -4,20 +4,15 @@ const withAuth = require("../utils/auth");
 
 
 // Michaels Codng Section:
-router.get("/", async (req, res) => {
+router.get("/games", async (req, res) => {
   try {
-    const gameData = await Game.findAll({
-      include: [
-        {
-          attributes: ["cover_art"],
-        },
-      ],
-    });
-//zz - try games ?
-    const games = gameData.get({ plain: true });
-    res.render("homepage", {
-      ...games,
-//zz - ...games, 
+
+    const gameData = await Game.findAll();
+    const games = gameData.map((games) =>
+    games.get({ plain: true }));
+    res.render("games", {
+      games,
+
     });
   } catch (err) {
     res.status(500).json(err);
@@ -110,6 +105,8 @@ router.get('/login', (req, res) => {
 
 //Member Zori's coding area
 
+
+
 // get specific tournament
 // URL should be /tournaments/games/game_id ?
 router.get("/tournament/:id", async (req, res) => {
@@ -159,6 +156,59 @@ router.get("/tournaments/", async (req, res) => {
     res.status(500).json(err);
   }
 });
+
+//-----------------old get routes-------------------
+// get specific tournament
+// // URL should be /tournaments/games/game_id ?
+// router.get("/tournament/:id", async (req, res) => {
+//   try {
+//     const tournamentData = await Tournament.findByPk(req.params.id, {
+//       include: [
+//         {
+//           model: Player,
+//           attributes: ["name"],
+//         },
+//       ],
+//     });
+
+//     const tournament = tournamentData.get({ plain: true });
+
+//     res.render("tournament", {
+//       ...tournament,
+//     });
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
+
+
+// //get all tournaments
+// router.get("/tournaments/", async (req, res) => {
+//   try {
+//     // Get all tournaments and JOIN with player data
+//     const tournamentData = await Tournament.findAll({
+//       // include: [
+//         // {
+//         //   model: Player,
+//         //   attributes: ["name"],
+//         // },
+//       // ],
+//     });
+
+//     const tournaments = tournamentData.map((tournament) =>
+//       tournament.get({ plain: true })
+//     );
+
+//     res.render("allTournaments", {
+//       tournaments,
+//       // logged_in: req.session.logged_in
+//     });
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
+
+//----------------------------------------------------------------
 
 
 
