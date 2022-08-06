0421e0bee76a4d6b5a646d51cc0a084aa71624f3
genrosales11-minjcA <genrosales11@gmail.com>
2022-08-05 17:55:48 -0700

    adding players login homeRoutes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -28,9 +28,85 @@ router.get("/", async (req, res) => {
 });
 
 
-//Member Genesis's coding area 
+//Member Genesis's coding area --------------------------- login ------------
+router.get('/', async (req, res) => {
+  try {
+    // Get all projects and JOIN with user data
+    const playerData = await player.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ['name'],
+        },
+      ],
+    });
 
+    // Serialize data so the template can read it
+    const player = playerData.map((player) => player.get({ plain: true }));
 
+    // Pass serialized data and session flag into template
+    res.render('homepage', { 
+      player, 
+      logged_in: req.session.logged_in 
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/player/:id', async (req, res) => {
+  try {
+    const playerData = await player.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          attributes: ['name'],
+        },
+      ],
+    });
+
+    const player = playerData.get({ plain: true });
+
+    res.render('homepage', {
+      ...player,
+      logged_in: req.session.logged_in
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Use withAuth middleware to prevent access to route
+router.get('/models/Game.js', withAuth, async (req, res) => {
+  try {
+    // Find the logged in user based on the session ID
+    const gameData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ['password'] },
+      include: [{ model: player }],
+    });
+
+    const game = gameData.get({ plain: true });
+
+    res.render('homepage', {
+      ...user,
+      logged_in: true
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/login', (req, res) => {
+  // If the user is already logged in, redirect the request to another route
+  if (req.session.logged_in) {
+    res.redirect('/models/Game.js');
+    return;
+  }
+
+  res.render('login');
+});
+
+//------------------------------------------------------------------------------------------
 
 
 
