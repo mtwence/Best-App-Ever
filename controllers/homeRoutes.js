0dc1cdea6edde6283c3d2a6aabae810265688afb
zzzorigtbaatar <zzzorigtbaatar@gmail.com>
2022-08-08 11:48:03 -0700

    add logout button

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -34,7 +34,7 @@ router.get('/games/:id/tournaments', async (req, res) => {
 
     res.render('game', {
       ...game,
-      // logged_in: req.session.logged_in
+      logged_in: req.session.logged_in
     });
   } catch (err) {
     res.status(500).json(err);
@@ -134,56 +134,6 @@ router.get("/login", (req, res) => {
 
 //Member Zori's coding area
 
-//defunct routes
-// get specific tournament
-// URL should be /tournaments/games/game_id ?
-// router.get("/tournament/:id", async (req, res) => {
-//   try {
-//     const tournamentData = await Tournament.findByPk(req.params.id, {
-//       include: [
-//         {
-//           model: Player,
-//           attributes: ["name"],
-//         },
-//       ],
-//     });
-
-//     const tournament = tournamentData.get({ plain: true });
-
-//     res.render("tournament", {
-//       ...tournament,
-//     });
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
-
-//get all tournaments
-// router.get("/tournaments/", async (req, res) => {
-//   try {
-//     // Get all tournaments and JOIN with player data
-//     const tournamentData = await Tournament.findAll({
-//       // include: [
-//       // {
-//       //   model: Player,
-//       //   attributes: ["name"],
-//       // },
-//       // ],
-//     });
-
-//     const tournaments = tournamentData.map((tournament) =>
-//       tournament.get({ plain: true })
-//     );
-
-//     res.render("allTournaments", {
-//       tournaments,
-//       // logged_in: req.session.logged_in
-//     });
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
-
 // get specific tournament
 // URL should be /tournaments/games/game_id ?
 router.get("/tournament/:id", async (req, res) => {
@@ -205,6 +155,7 @@ router.get("/tournament/:id", async (req, res) => {
 
     res.render("tournament", {
       ...tournament,
+      logged_in: req.session.logged_in
     });
   } catch (err) {
     res.status(500).json(err);
@@ -234,7 +185,7 @@ router.get("/tournaments/", async (req, res) => {
 
     res.render("allTournaments", {
       tournaments,
-      // logged_in: req.session.logged_in
+      logged_in: req.session.logged_in
     });
   } catch (err) {
     res.status(500).json(err);
