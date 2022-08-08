67ba5d5d4e0cf4c8d58b990314025dc06c2ae995
genrosales11-minjcA <genrosales11@gmail.com>
2022-08-07 18:07:44 -0700

    main and login handlebars

--- public/js/login.js
+++ public/js/login.js
@@ -2,15 +2,15 @@ const loginFormHandler = async (event) => {
     event.preventDefault();
   
     // Collect values from the login form
-    const email = document.querySelector('#email-login').value.trim();
-    const password = document.querySelector('#password-login').value.trim();
+    const email = document.querySelector('email-login').value.trim();
+    const password = document.querySelector('password-login').value.trim();
   
     if (email && password) {
       // Send a POST request to the API endpoint
-      const response = await fetch('/api/players/login', {
+      const response = await fetch('/api/games/login', {
         method: 'POST',
         body: JSON.stringify({ email, password }),
-        headers: { 'Content-Type': 'application/json' },
+        // headers: { 'Content-Type': 'application/json' },
       });
   
       if (response.ok) {
@@ -30,14 +30,14 @@ const loginFormHandler = async (event) => {
     const password = document.querySelector('#password-signup').value.trim();
   
     if (name && email && password) {
-      const response = await fetch('/api/players', {
+      const response = await fetch('/api/games', {
         method: 'POST',
         body: JSON.stringify({ name, email, password }),
         headers: { 'Content-Type': 'application/json' },
       });
   
       if (response.ok) {
-        document.location.replace('/games'); //---- Gamee will be home page-------------
+        document.location.replace('/login'); //---- Gamee will be home page-------------
       } else {
         alert(response.statusText);
       }
@@ -45,9 +45,10 @@ const loginFormHandler = async (event) => {
   };
   
   document
-    .querySelector('.login-form')
-    .addEventListener('submit', loginFormHandler);
-  
-  document
-    .querySelector('.signup-form')
-    .addEventListener('submit', signupFormHandler);
+  .querySelector('.login-form')
+  .addEventListener('submit', loginFormHandler);
+
+document
+  .querySelector('.signup-form')
+  .addEventListener('submit', signupFormHandler);
+
