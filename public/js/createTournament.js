185f841936f958d4e032ce1f0a5e4de70a38aa91
Gleepy <107324298+mtwence@users.noreply.github.com>
2022-08-11 02:30:47 -0400

    fixing merge issues

--- public/js/createTournament.js
+++ public/js/createTournament.js
@@ -2,48 +2,30 @@ const newFormHandler = async (event) => {
   event.preventDefault();
 
   const tournament_name = document.querySelector('#tournament-name').value.trim();
+  const tournament_date = document.querySelector('#tournament-date').value.trim();
   const tournament_players = document.querySelector('#tournament-players').value.trim();
   const description = document.querySelector('#tournament-desc').value.trim();
-  const inputGame = document.getElementById('#inputGame').value;
-
-  if (tournament_name && tournament_players && description && inputGame) {
+  const inputGame = document.getElementById('inputGame').value;
+  const discord = document.querySelector('#tournament-disc').value.trim();
+  const host = document.querySelector('#host').value.trim();
+  
+  if (tournament_name && tournament_players && description && inputGame && discord && tournament_date && host) {
     console.log(inputGame);
     const response = await fetch(`/api/tournaments`, {
       method: 'POST',
-      body: JSON.stringify({ tournament_name: tournament_name, player_quantity: tournament_players, description: description, game_id: inputGame}),
+      body: JSON.stringify({ tournament_name: tournament_name, player_quantity: tournament_players, description: description, game_id: inputGame, discord_link: discord, event_time: tournament_date, host: host}),
       headers: {
         'Content-Type': 'application/json',
       },
     });
     console.log(response)
     if (response.ok) {
-      document.location.replace('/all-tournaments');
+      document.location.replace(`/games/${inputGame}/tournaments`);
     } else {
       alert('Failed to create tournament');
     }
   }
 };
-
-const delButtonHandler = async (event) => {
-  if (event.target.hasAttribute('data-id')) {
-    const id = event.target.getAttribute('data-id');
-
-    const response = await fetch(`/api/tournaments/${id}`, {
-      method: 'DELETE',
-    });
-
-    if (response.ok) {
-      document.location.replace('/newTournament');
-    } else {
-      alert('Failed to delete tournament');
-    }
-  }
-};
-
 document
   .querySelector('.new-tournament-form')
   .addEventListener('submit', newFormHandler);
-
-document
-  .querySelector('.tournament-list')
-  .addEventListener('click', delButtonHandler);
